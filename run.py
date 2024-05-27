be36a4321fdb92e33e8a028ba9fee896a6a2f33c
ioterw <you@example.com>
2024-05-27 20:50:26 +0400

    removed garbage code

--- run.py
+++ run.py
@@ -10,25 +10,22 @@ def popen(*args, **kwargs):
     return res
 
 def handle_config(config_path):
-    remove_db_path = None
-
     json_folder = os.path.abspath(config_path).rsplit('/', 1)[0]
     with open(config_path, 'r') as f:
         config = json.load(f)
     if config['kv']['engine'] == 'leveldb':
         if not config['kv']['root'].startswith('/'):
             config['kv']['root'] = json_folder + '/' + config['kv']['root']
-        remove_db_path = config['kv']['root']
     if 'output' in config and config['output'] != '' and not config['output'].startswith('/'):
         config['output'] = json_folder + '/' + config['output']
-    return config, remove_db_path
+    return config
 
 def run_server(config):
     app_path = os.path.dirname(os.path.abspath(__file__)) + '/webview/app.py'
     p = subprocess.Popen(['python3', app_path, config['output']], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return p
 
-def run_geth(config, remove_db_path):
+def run_geth(config):
     vmtrace_option = []
     geth_path = os.path.dirname(os.path.abspath(__file__)) + '/geth'
     popen([
@@ -39,11 +36,11 @@ def run_geth(config, remove_db_path):
     ])
 
 def main(config_path):
-    config, remove_db_path = handle_config(config_path)
+    config = handle_config(config_path)
     try:
         p = run_server(config)
         webbrowser.open('http://127.0.0.1:4334', new=0, autoraise=True)
-        run_geth(config, remove_db_path)
+        run_geth(config)
     finally:
         p.kill()
 
