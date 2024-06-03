6d8e9920dd9292a06505e8bff63688dbb32b2cfb
ioterw <iotrwewe12@protonmail.com>
2024-06-03 23:33:27 +0400

    removed geth rlp

--- tracer/dep_tracer/crypto_helpers.go
+++ tracer/dep_tracer/crypto_helpers.go
@@ -2,14 +2,38 @@ package dep_tracer
 
 import (
     "hash"
+    "encoding/binary"
     "golang.org/x/crypto/sha3"
-    
-    "github.com/ethereum/go-ethereum/rlp"
 )
 
-func CreateAddress(b Address, nonce uint64) Address {
-    data, _ := rlp.EncodeToBytes([]interface{}{b, nonce})
+func CreateAddress(caller Address, nonce uint64) Address {
+    data := rlpEncodeCreate(caller, nonce)
     return bytesToAddress(Keccak256(data)[12:])
+}
+
+func rlpEncodeCreate(caller Address, nonce uint64) []byte {
+    res := []byte{}
+    res = append(res, rlpEncodeAddress(caller)...)
+    res = append(res, rlpEncodeNonce(nonce)...)
+    var prefix byte = 0xc0 + byte(len(res))
+    return append([]byte{prefix}, res...)
+}
+
+func rlpEncodeAddress(addr Address) []byte {
+    var prefix byte = 0x94
+    return append([]byte{prefix}, addr[:]...)
+}
+
+func rlpEncodeNonce(nonce uint64) []byte {
+    res := binary.BigEndian.AppendUint64(nil, nonce)
+    for len(res) > 0 && res[0] == 0 {
+        res = res[1:]
+    }
+    if len(res) == 1 && 0x00 <= res[0] && res[0] <= 0x7f {
+        return res
+    }
+    var prefix byte = 0x80 + byte(len(res))
+    return append([]byte{prefix}, res...)
 }
 
 func CreateAddress2(b Address, salt Hash, inithash []byte) Address {
