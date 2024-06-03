b31a59368c6ad6feaca244fc8ad1b80a4456ef23
ioterw <iotrwewe12@protonmail.com>
2024-06-04 02:23:18 +0400

    prepare for standalone lib

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import os, subprocess, shutil, glob
+import os, subprocess, shutil, glob, sys
 
 def popen(*args, **kwargs):
     p = subprocess.Popen(*args, **kwargs)
@@ -8,16 +8,16 @@ def popen(*args, **kwargs):
         exit(1)
     return res
 
-def main():
-    target = 'geth'
-    
+def main(target):
     if target == 'geth':
         os.chdir(os.path.dirname(os.path.abspath(__file__)) + '/go-ethereum')
 
         shutil.rmtree('eth/tracers/live/dep_tracer',  ignore_errors=True)
         shutil.rmtree('eth/tracers/live/dep_geth.go', ignore_errors=True)
 
-        shutil.copytree('../tracer/dep_tracer', 'eth/tracers/live/dep_tracer')
+        os.mkdir('eth/tracers/live/dep_tracer')
+        for path in glob.iglob('../tracer/dep_tracer/*.go'):
+            shutil.copy(path, 'eth/tracers/live/dep_tracer/')
         shutil.copy('../tracer/dep_geth.go', 'eth/tracers/live/dep_geth.go')
 
         popen(['go', 'get', 'github.com/basho/riak-go-client'])
@@ -25,15 +25,27 @@ def main():
         shutil.copy('build/bin/geth', '..')
 
         os.chdir('..')
-        shutil.rmtree('build', ignore_errors=True)
-        os.mkdir('build')
+        try:
+            os.mkdir('build')
+        except FileExistsError:
+            pass
         shutil.copy('run.py', 'build')
         shutil.copy('geth', 'build')
         shutil.copy('conf_examples/default.json', 'conf.json')
         shutil.copy('conf.json', 'build')
+    elif target == 'lib':
+        os.chdir(os.path.dirname(os.path.abspath(__file__)) + '/tracer/dep_tracer')
+
+        try:
+            os.mkdir('../../build')
+        except FileExistsError:
+            pass
+        
+        popen(['go', 'build', '-buildmode=archive', '-o', '../../build/lib.a'])
     else:
         print('Unknown target:', target)
+        print('Targets: geth, lib')
         exit(1)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
---
+++ tracer/dep_tracer/go.mod
@@ -0,0 +1,17 @@
+module dep_tracer
+
+go 1.22.3
+
+require (
+	github.com/basho/riak-go-client v1.7.0
+	github.com/holiman/uint256 v1.2.4
+	github.com/syndtr/goleveldb v1.0.0
+	golang.org/x/crypto v0.23.0
+)
+
+require (
+	github.com/basho/backoff v0.0.0-20150307023525-2ff7c4694083 // indirect
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	golang.org/x/sys v0.20.0 // indirect
+)
